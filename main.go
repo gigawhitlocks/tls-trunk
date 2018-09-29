28ce9b47a5723a5f0213b2b099e6d4db0934fdbf
Ian Whitlock <ian@theknown.net>
2018-09-29 01:46:23 -0500

    working PoC TLS authenticating trunk proxy

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"net"
+	"os"
+)
+
+func chanFromConn(conn net.Conn) chan []byte {
+	c := make(chan []byte)
+	go func() {
+		for {
+			b := make([]byte, 1024)
+			n, err := conn.Read(b)
+			if n > 0 {
+				res := make([]byte, n)
+				copy(res, b[:n])
+				c <- res
+			}
+			if err != nil {
+				c <- nil
+				break
+			}
+		}
+	}()
+	return c
+}
+
+func main() {
+	certificate, err := tls.LoadX509KeyPair("/home/ian/ian.crt.pem",
+		"/home/ian/ian.key.pem")
+	if err != nil {
+		fmt.Printf("Failed to load certs: %s\n", err)
+		os.Exit(1)
+	}
+
+	config := &tls.Config{
+		Certificates: []tls.Certificate{certificate},
+	}
+
+	ln, err := net.Listen("tcp", ":8080")
+	fmt.Printf("Listening on :8080\n")
+	if err != nil {
+		os.Exit(1)
+	}
+
+	for {
+		c, err := ln.Accept()
+		if err != nil {
+			fmt.Printf("Failed to accept: %s\n", err)
+			continue
+		}
+
+		client, err := tls.Dial("tcp", "secret.theknown.net:443", config)
+		if err != nil {
+			fmt.Printf("Failed to connect: %s\n", err)
+			os.Exit(1)
+		}
+
+		upstream := chanFromConn(client)
+		local := chanFromConn(c)
+
+		go func() {
+			for {
+				select {
+				case b1 := <-upstream:
+					if b1 == nil {
+						return
+					}
+					c.Write(b1)
+				case b2 := <-local:
+					if b2 == nil {
+						return
+					}
+					client.Write(b2)
+				}
+			}
+		}()
+	}
+}
